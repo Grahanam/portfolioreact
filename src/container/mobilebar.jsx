fa59e546492bc58c55a9c699ff68139d367f35a0
grahanam <lunasuthar5221@gmail.com>
2022-11-10 11:47:52 +0530

    third commit

--- src/container/mobilebar.jsx
+++ src/container/mobilebar.jsx
@@ -12,31 +12,32 @@ const Bottombar = ()=>{
         <>
         <div className=" w-full bg-black block absolute bottom-0 md:hidden lg:hidden">
             <div className=" flex mx-1 my-1 justify-evenly">
-                <NavLink to="/" className="" activeClassName="active" >
-                  <button className={`w-full text-xl font-semibold text-${location==='/'?'white':'lightest'} hover:text-yellow-300  px-1 py-2 items-center justify-start`}>
+                <NavLink to="/" className=""  >
+                {/* ${location==='/'?'white':'lightest'} */}
+                  <button className={'w-full text-xl font-semibold text-lightest hover:text-yellow-300  px-1 py-2 items-center justify-start'}>
                   <i className=""><FontAwesomeIcon icon={faHome} className="mx-2"/></i>
                     {/* <p>Home</p> */}
                   </button>
                 </NavLink>
-                <NavLink to="/about" className="" activeClassName="active" >
-                  <button className={`w-full text-xl font-semibold text-${location==='/about'?'white':'lightest'} hover:text-yellow-300  px-1 py-2 items-center justify-start`}>
+                <NavLink to="/about" className="" >
+                  <button className={'w-full text-xl font-semibold text-lightest hover:text-yellow-300  px-1 py-2 items-center justify-start'}>
                   <i className=""><FontAwesomeIcon icon={faUserAstronaut} className="mx-2"/></i>
                     {/* <p>About</p> */}
                   </button>
                 </NavLink>
-                <NavLink to="/skill" className="" activeClassName="active" >
+                <NavLink to="/skill" className=""  >
                   <button className={`w-full text-xl font-semibold text-lightest hover:text-yellow-300  px-1 py-2  items-center justify-start`}>
                     <i className=""><FontAwesomeIcon icon={faFire} className="mx-2"/></i>
                     {/* <p>Project</p> */}
                   </button>
                 </NavLink>
-                <NavLink to="/project" className="" activeClassName="active" >
+                <NavLink to="/project" className=""  >
                   <button className={`w-full text-xl font-semibold text-lightest hover:text-yellow-300  px-1 py-2  items-center justify-start`}>
                     <i className=""><FontAwesomeIcon icon={faBarChart} className="mx-2"/></i>
                     {/* <p>Project</p> */}
                   </button>
                 </NavLink>
-                <NavLink to="/contact" className="" activeClassName="active" >
+                <NavLink to="/contact" className=""  >
                   <button className={`w-full text-xl font-semibold text-lightest hover:text-yellow-300  px-1 py-2  items-center justify-start`}>
                     <i className=""><FontAwesomeIcon icon={faContactCard} className="mx-2"/></i>
                     {/* <p>Project</p> */}
