4739872fd0aa680a38d8cfd599ab68e1ae4ab8d1
grahanam <lunasuthar5221@gmail.com>
2023-03-03 19:56:41 +0530

    added projects

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -2,7 +2,7 @@ import React,{useState} from "react"
 import {Canvas} from "@react-three/fiber"
 import AnimatedSphere from "../component/object"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faP,faHeartMusicCameraBolt,faListCheck} from '@fortawesome/free-solid-svg-icons'
+import { faP,faHeartMusicCameraBolt,faListCheck,faMaskFace,faFaceMehBlank} from '@fortawesome/free-solid-svg-icons'
 import AnimatedLetter from "../component/animatedletter/letter"
 import '../app.scss'
 import '../component/animatedletter/letter.scss'
@@ -55,20 +55,29 @@ const Project=()=>{
            </div>
            </div>
            <div className="container w-full flex flex-wrap">
-                {/* <a href='http://43.204.233.102/' target="_blank" rel="noreferrer"><div className='h-32 w-32  md:h-36 md:w-36 lg:h-44 lg:w-44 m-4 rounded '>
-                          <div className='h-full w-full rounded flex items-center justify-center bg-lime-500 '>
-                            <h1 className='absolute font-extrabold text-3xl md:text-4xl lg:text-5xl p-2 text-white'>Do-List</h1>
-                            <i className="text-6xl md:text-7xl lg:text-8xl hover:text-yellow-400 duration-75"><FontAwesomeIcon icon={faListCheck}  className=""/></i>
-                          </div>
-                          <div className=''></div>
-                </div>
-                </a> */}
+                
                 <a href='http://aura.pythonanywhere.com/' target="_blank" rel="noreferrer">
                 <div className='h-32 w-32 md:h-36 md:w-36 lg:h-44 lg:w-44 m-4 rounded'>
                           <div className='h-full w-full bg-no-repeat bg-blue-500 rounded flex items-center justify-center' >
                             <h1 className='absolute font-extrabold text-3xl md:text-4xl lg:text-5xl p-2 text-white'>Aura</h1>
                             <i className="text-6xl md:text-7xl lg:text-8xl hover:text-yellow-400 duration-75"><FontAwesomeIcon icon={faHeartMusicCameraBolt}  className=""/></i>
                             </div>
+                          <div className=''></div>
+                </div>
+                </a>
+                <a href='https://grahanam.github.io/faceio/' target="_blank" rel="noreferrer"><div className='h-32 w-32  md:h-36 md:w-36 lg:h-44 lg:w-44 m-4 rounded '>
+                          <div className='h-full w-full rounded flex items-center justify-center bg-lime-500 '>
+                            <h1 className='absolute font-extrabold text-3xl md:text-4xl lg:text-5xl p-2 text-white'>Faceio</h1>
+                            <i className="text-6xl md:text-7xl lg:text-8xl hover:text-yellow-400 duration-75"><FontAwesomeIcon icon={faMaskFace}  className=""/></i>
+                          </div>
+                          <div className=''></div>
+                </div>
+                </a>
+                <a href='https://grahanam.github.io/facerecognition' target="_blank" rel="noreferrer"><div className='h-32 w-32  md:h-36 md:w-36 lg:h-44 lg:w-44 m-4 rounded '>
+                          <div className='h-full w-full rounded flex items-center justify-center bg-lime-500 '>
+                            <h1 className='absolute font-extrabold text-3xl md:text-4xl lg:text-5xl p-2 text-white'>FRT</h1>
+                            <i className="text-6xl md:text-7xl lg:text-8xl hover:text-yellow-400 duration-75"><FontAwesomeIcon icon={faFaceMehBlank}  className=""/></i>
+                          </div>
                           <div className=''></div>
                 </div>
                 </a>
